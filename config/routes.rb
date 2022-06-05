75020cc51fa16e87ff5f245dc510b62b03db515e
ryo246912 <r-k.sky_25cloudf@hotmail.co.jp>
2022-06-05 21:14:56 +0900

    [add]: MVC周りを一通り操作
    RailsガイドをもとにMVC周りを一通り操作

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   resources :users
+  resources :articles do
+    resources :comments
+  end
   root 'application#hello'
+  # get "articles", to: "articles#index"
+  # get "articles/:id", to: "articles#show"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
